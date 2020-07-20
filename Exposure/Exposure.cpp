7f16e983ed44ec47605da063167f19389e3f37cd
Nate Lovell <Natelovell99@gmail.com>
2020-07-20 00:40:27 -0600

    All Files

---
+++ Exposure/Exposure.cpp
@@ -0,0 +1,241 @@
+/*******************************************************************/
+/*                                                                 */
+/*                      ADOBE CONFIDENTIAL                         */
+/*                   _ _ _ _ _ _ _ _ _ _ _ _ _                     */
+/*                                                                 */
+/* Copyright 2007 Adobe Systems Incorporated                       */
+/* All Rights Reserved.                                            */
+/*                                                                 */
+/* NOTICE:  All information contained herein is, and remains the   */
+/* property of Adobe Systems Incorporated and its suppliers, if    */
+/* any.  The intellectual and technical concepts contained         */
+/* herein are proprietary to Adobe Systems Incorporated and its    */
+/* suppliers and may be covered by U.S. and Foreign Patents,       */
+/* patents in process, and are protected by trade secret or        */
+/* copyright law.  Dissemination of this information or            */
+/* reproduction of this material is strictly forbidden unless      */
+/* prior written permission is obtained from Adobe Systems         */
+/* Incorporated.                                                   */
+/*                                                                 */
+/*******************************************************************/
+
+/*	Vignette.cpp	
+
+	This is a compiling husk of a project. Fill it in with interesting
+	pixel processing code.
+	
+	Revision History
+
+	Version		Change													Engineer	Date
+	=======		======													========	======
+	1.0			(seemed like a good idea at the time)					bbb			6/1/2002
+
+	1.0			Okay, I'm leaving the version at 1.0,					bbb			2/15/2006
+				for obvious reasons; you're going to 
+				copy these files directly! This is the
+				first XCode version, though.
+
+	1.0			Let's simplify this barebones sample					zal			11/11/2010
+
+	1.0			Added new entry point									zal			9/18/2017
+
+*/
+
+#include "Exposure.h"
+
+static PF_Err 
+About (	
+	PF_InData		*in_data,
+	PF_OutData		*out_data,
+	PF_ParamDef		*params[],
+	PF_LayerDef		*output )
+{
+	AEGP_SuiteHandler suites(in_data->pica_basicP);
+	
+	suites.ANSICallbacksSuite1()->sprintf(	out_data->return_msg,
+											"%s v%d.%d\r%s",
+											STR(StrID_Name), 
+											MAJOR_VERSION, 
+											MINOR_VERSION, 
+											STR(StrID_Description));
+	return PF_Err_NONE;
+}
+
+static PF_Err 
+GlobalSetup (	
+	PF_InData		*in_data,
+	PF_OutData		*out_data,
+	PF_ParamDef		*params[],
+	PF_LayerDef		*output )
+{
+	out_data->my_version = PF_VERSION(	MAJOR_VERSION, 
+										MINOR_VERSION,
+										BUG_VERSION, 
+										STAGE_VERSION, 
+										BUILD_VERSION);
+
+	out_data->out_flags =  PF_OutFlag_DEEP_COLOR_AWARE;	// just 16bpc, not 32bpc
+	
+	return PF_Err_NONE;
+}
+
+static PF_Err 
+ParamsSetup (	
+	PF_InData		*in_data,
+	PF_OutData		*out_data,
+	PF_ParamDef		*params[],
+	PF_LayerDef		*output )
+{
+	PF_Err		err		= PF_Err_NONE;
+	PF_ParamDef	def;	
+
+	AEFX_CLR_STRUCT(def);
+
+	PF_ADD_FLOAT_SLIDERX(	STR(StrID_Exp_Param_Name),
+							PF_FpShort(0.001), 
+								PF_FpShort(3.000),
+									PF_FpShort(0.001),
+										PF_FpShort(3.000),
+											PF_FpShort(1.000),
+							PF_Precision_THOUSANDTHS,
+							0,
+							0,
+							EXPOSURE_DISK_ID);
+
+	
+	out_data->num_params = EXPOSURE_NUM_PARAMS;
+
+	return err;
+}
+
+static PF_Err
+ExpFunc8 (
+	void		*refcon, 
+	A_long		xL, 
+	A_long		yL, 
+	PF_Pixel8	*inP, 
+	PF_Pixel8	*outP)
+{
+	PF_Err		err = PF_Err_NONE;
+
+	ExpInfo	*giP	= reinterpret_cast<ExpInfo*>(refcon);
+	PF_FpLong	exp	= 0;
+					
+	if (giP){
+		exp = giP->exposure;
+
+		outP->alpha		=	inP->alpha;
+		outP->red		=	A_u_char(inP->red * exp);
+		outP->green		=	A_u_char(inP->green * exp);
+		outP->blue		=	A_u_char(inP->blue * exp);
+	}
+
+	return err;
+}
+
+static PF_Err 
+Render (
+	PF_InData		*in_data,
+	PF_OutData		*out_data,
+	PF_ParamDef		*params[],
+	PF_LayerDef		*output )
+{
+	PF_Err				err		= PF_Err_NONE;
+	AEGP_SuiteHandler	suites(in_data->pica_basicP);
+
+	/*	Put interesting code here. */
+	ExpInfo			giP;
+	AEFX_CLR_STRUCT(giP);
+	A_long				linesL	= 0;
+
+	linesL 		= output->extent_hint.bottom - output->extent_hint.top;
+	giP.exposure 	= params[EXPOSURE_EXPOSURE]->u.fs_d.value;
+	
+		ERR(suites.Iterate8Suite1()->iterate(	in_data,
+												0,								// progress base
+												linesL,							// progress final
+												&params[EXPOSURE_INPUT]->u.ld,	// src 
+												NULL,							// area - null for all pixels
+												(void*)&giP,					// refcon - your custom data pointer
+			ExpFunc8,				// pixel function pointer
+												output));	
+	
+
+	return err;
+}
+
+
+extern "C" DllExport
+PF_Err PluginDataEntryFunction(
+	PF_PluginDataPtr inPtr,
+	PF_PluginDataCB inPluginDataCallBackPtr,
+	SPBasicSuite* inSPBasicSuitePtr,
+	const char* inHostName,
+	const char* inHostVersion)
+{
+	PF_Err result = PF_Err_INVALID_CALLBACK;
+
+	result = PF_REGISTER_EFFECT(
+		inPtr,
+		inPluginDataCallBackPtr,
+		"Vignette", // Name
+		"ADBE Vignette", // Match Name
+		"Sample Plug-ins", // Category
+		AE_RESERVED_INFO); // Reserved Info
+
+	return result;
+}
+
+
+PF_Err
+EffectMain(
+	PF_Cmd			cmd,
+	PF_InData		*in_data,
+	PF_OutData		*out_data,
+	PF_ParamDef		*params[],
+	PF_LayerDef		*output,
+	void			*extra)
+{
+	PF_Err		err = PF_Err_NONE;
+	
+	try {
+		switch (cmd) {
+			case PF_Cmd_ABOUT:
+
+				err = About(in_data,
+							out_data,
+							params,
+							output);
+				break;
+				
+			case PF_Cmd_GLOBAL_SETUP:
+
+				err = GlobalSetup(	in_data,
+									out_data,
+									params,
+									output);
+				break;
+				
+			case PF_Cmd_PARAMS_SETUP:
+
+				err = ParamsSetup(	in_data,
+									out_data,
+									params,
+									output);
+				break;
+				
+			case PF_Cmd_RENDER:
+
+				err = Render(	in_data,
+								out_data,
+								params,
+								output);
+				break;
+		}
+	}
+	catch(PF_Err &thrown_err){
+		err = thrown_err;
+	}
+	return err;
+}
+
